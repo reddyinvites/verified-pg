0fd00ab3b6721624a24ff98438338e5ecf860cd9
reddyinvites <reddyinvites@gmail.com>
2026-04-02 10:40:29 +0530

    Update app.py

--- app.py
+++
@@ -1,5 +0,0 @@
-cloudinary.config(
-    cloud_name=st.secrets["cloudinary"]["cloud_name"],
-    api_key=st.secrets["cloudinary"]["api_key"],
-    api_secret=st.secrets["cloudinary"]["api_secret"]
-)
